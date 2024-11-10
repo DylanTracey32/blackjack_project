3d1b7bf3576a502d6489f883581a7ef563063f02
Dylan Tracey <dylan.tracey324@gmail.com>
2024-11-09 22:27:01 -0330

    Added deck module which creates a 2D list with 52 cards, each card a list that contains the rank, suit, and value, shuffled in a random order.

---
+++ deck.py
@@ -0,0 +1,38 @@
+import random
+
+def get_ranks():
+    return ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+def get_suits():
+    return ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+def generate_deck(ranks, suits):
+    deck = []
+    for suit in suits:
+        for rank in ranks:
+            deck.append([rank, suit, None])
+    return deck
+
+def assign_values(deck):
+    for card in deck:
+        if card[0].isdigit():
+            card[2] = int(card[0])
+        elif card[0] in ["Jack", "Queen", "King"]:
+            card[2] = 10
+        elif card[0] == "Ace":
+            card[2] = [1, 11]
+
+def shuffle(deck):
+    random.shuffle(deck)
+    return(deck)
+
+def main():
+    ranks = get_ranks()
+    suits = get_suits()
+    deck = generate_deck(ranks, suits)
+    assign_values(deck)
+    shuffle(deck)
+    return(deck)
+
+if __name__ == "__main__":
+    main()
