4970699c59dbb336e02e1f21645ceaa55c0b4f2d
Dylan Tracey <dylan.tracey324@gmail.com>
2024-11-21 21:30:14 -0330

    added commands and new functions which aren't operational yet without db.py.

--- game_logic.py
+++ game_logic.py
@@ -1,12 +1,22 @@
 import deck
 
-def starting_hand(game_deck, player_hand, dealer_hand):
-    player_hand.append(game_deck.pop(0))
-    dealer_hand.append(game_deck.pop(0))
-    player_hand.append(game_deck.pop(0))
-    dealer_hand.append(game_deck.pop(0))
+def title():
+    print("BLACKJACK!\n"
+          "Blackjack payout is 3:2\n")
+    
+def show_balance(money):
+    print(f"Money:")
 
-def check_value(hand):
+def deal_card(game_deck): #deals a card
+    return game_deck.pop()
+
+def starting_hand(game_deck, player_hand, dealer_hand): # starting hand for player and dealer
+    player_hand.append(deal_card(game_deck))
+    dealer_hand.append(deal_card(game_deck))
+    player_hand.append(deal_card(game_deck))
+    dealer_hand.append(deal_card(game_deck))
+
+def check_value(hand): # return player or dealer's hand
     hand_value = 0
     aces = 0
 
@@ -38,16 +48,19 @@ def main():
     bet = 0
 
     while True:
+        title()
+        show_balance()
         starting_hand(game_deck, player_hand, dealer_hand)
         player_value = check_value(player_hand)
         dealer_value = check_value(dealer_hand)
         print("YOUR CARDS:")
+        display_cards()
         if win_check(player_value) == True:
             bet *= 1.5
             money += bet
             print("You got blackjack!\n")
         else:
-            print("")
+
 
         
         
