2a14ba75aca1fdaa97ec8fe3194832931cdee814
Dylan Tracey <dylan.tracey324@gmail.com>
2024-11-25 21:52:34 -0330

    Revert "Features"

--- deck.py
+++ deck.py
@@ -1,5 +1,3 @@
-# This module's sole purpose is to provide a random card
-
 import random
 
 def get_ranks():
@@ -8,14 +6,14 @@ def get_ranks():
 def get_suits():
     return ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-def generate_deck(ranks, suits): # Initializes deck and assigns suit and rank
+def generate_deck(ranks, suits):
     deck = []
     for suit in suits:
         for rank in ranks:
-            deck.append([rank, suit, None]) # "None" is a work-around for the dual ace value
+            deck.append([rank, suit, None])
     return deck
 
-def assign_values(deck): # Assigns value to the deck
+def assign_values(deck):
     for card in deck:
         if card[0].isdigit():
             card[2] = int(card[0])
@@ -24,15 +22,17 @@ def assign_values(deck): # Assigns value to the deck
         elif card[0] == "Ace":
             card[2] = [1, 11]
 
-def random_card(deck):
-    return random.choice(deck)
+def shuffle(deck):
+    random.shuffle(deck)
+    return(deck)
 
 def main():
     ranks = get_ranks()
     suits = get_suits()
     deck = generate_deck(ranks, suits)
     assign_values(deck)
-    return random_card(deck)
+    shuffle(deck)
+    return deck
 
 if __name__ == "__main__":
     main()
